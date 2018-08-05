17b80508f895dd1e098e5f892863107c755ff721
Jess <jessisawesome@hotmail.co.nz>
2018-08-05 18:40:44 +1200

    Switch label to pack instead of grid

--- AddALabel.py
+++ AddALabel.py
@@ -7,7 +7,11 @@ window = tk.Tk()
 window.title("Labels")
 
 #To add WIDGETS to the window we need to make sure that we are writing code AFTER the instance was created but BEFORE we display the window to the user
-#We are going to put a label inside our window
-label0 = ttk.Label(window, text="A Label").grid(column=0, row=0)
+#We are going to create a label to put inside our window
+label0 = ttk.Label(window, text="A Label")
+
+#So now we have a label, but we need to put it inside the window. For now we will do this by "packing"
+#This will automatically add our label to the bottom of the window, nice andd centered.
+label0.pack()
 
 window.mainloop()
